7a0b2fd52580763d379b9992e53c2b6dea65f0ff
Andy Shin <andrewkshin@gmail.com>
2012-06-27 00:46:02 -0700

    add address validation

---
+++ lib/active_shipping/shipping/address_validation_response.rb
@@ -0,0 +1,20 @@
+module ActiveMerchant
+  module Shipping
+    class AddressValidationResponse < Response
+
+      attr_reader :status_code
+      attr_reader :status_type
+      attr_reader :addresses
+
+      def initialize(success, message, params = {}, options = {})
+        @addresses = []
+        @status_type = options[:status_type]
+        @status_code = options[:status_code]
+        options[:addresses].each{ |address| @addresses << address } unless options[:address].blank?
+        super
+      end
+
+    end
+
+  end
+end
