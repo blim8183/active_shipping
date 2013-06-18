8c2782960c4db8f743f88f6bdd792cabed52fece
jdnguyen <jdnguyenxo@gmail.com>
2013-06-18 17:07:23 -0400

    added description on invoice

--- lib/active_shipping/shipping/carriers/ups.rb
+++ lib/active_shipping/shipping/carriers/ups.rb
@@ -139,6 +139,7 @@ module ActiveMerchant
           end
           root_node << XmlNode.new('Shipment') do |shipment|
             if packages[0] and packages[0].options[:products].length > 0
+              shipment << XmlNode.new('Description', "Clothes and clothing accessories")
               if options[:destination][:country] == "CA"
                 totalValue = 0
                 packages.each do |package|
